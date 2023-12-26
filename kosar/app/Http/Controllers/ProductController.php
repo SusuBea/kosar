ce91d1b4a29c229e17f3f87597298690438641d6
SuSuBea <suhajdabea>
2023-12-26 15:27:56 +0100

    egyszerű elsődleges kulcsok készen

---
+++ kosar/app/Http/Controllers/ProductController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index(){
+        $items = response()->json(Product::all());
+        return $items;
+    }
+
+    public function show($id){
+        $item = response()->json(Product::find($id));
+        return $item;
+    }
+
+    public function store(Request $request){
+        $item = new Product();
+        $item->type_id = $request->type_id;
+        $item->date = $request->date;
+    
+
+        $item->save();
+    }
+
+    public function update(Request $request, $id){
+        $item = Product::find($id);
+        $item->type_id = $request->type_id;
+        $item->date = $request->date;
+        
+        $item->save();
+    }
+
+    public function destroy($id){
+        Product::find($id)-> delete();
+    }
+}
